d81b2d8e4409042c415f0d2e868a635c4b2b2723
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-21 12:48:18 +0300

    🔨 Add error messages

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,14 +1,24 @@
 import { z } from 'zod'
 
 
-export const zName = z.string().min(2).max(64).regex(/^[A-Za-z' -]+$/) // Regex supports letters, ' - and space
-export const zEGN = z.string().length(10) // .regex(/^[0-9]+$/) // todo update regex with EGN rules
-export const zAddress = z.string().min(2).max(255)
-export const zPostcode = z.number().int().nonnegative().min(1000).max(9999)
-export const zPhoneNumber = z.string().min(9).max(16).regex(/^(?:\+|00)\d{9,16}$/) // Starts with + or 00 and
-// followed by
-// 9-16 digits
-export const zEmail = z.string().email()
+export const zName = z.string()
+  .min(2, { message: 'Name must be at least 2 characters long' })
+  .max(64, { message: 'Name must be at most 64 characters long' })
+  .regex(/^[A-Za-z' -]+$/, { message: 'Only letters, apostrophes, hyphens, and spaces are allowed' }) // Regex supports letters, ' - and space
+export const zEGN = z.string()
+  .length(10, { message: 'Maximum length of EGN is 10 characters' }) // .regex(/^[0-9]+$/) // todo update regex with EGN rules
+export const zAddress = z.string()
+  .min(2, { message: 'Address must be at least 2 characters long' })
+  .max(255, { message: 'Address must be at most 255 characters long' })
+export const zPostcode = z.string()
+  .length(4, { message: 'Postcode must be exactly 4 characters long' })
+  .regex(/\d{4}$/, { message: 'Postcode must be a 4-digit number' }) // Regex supports only 4 digits
+export const zPhoneNumber = z.string()
+  .min(9, { message: 'Phone number must be at least 9 characters long' })
+  .max(16, { message: 'Phone number must be at most 16 characters long' })
+  .regex(/^(?:\+|00)\d{9,16}$/, { message: 'Phone number must start with + or 00' }) // Starts with + or 00 and followed by 9 to 16 digits
+export const zEmail = z.string()
+  .email({ message: 'Email must be a valid email address' })
 
 export const zPayload = z.strictObject({
   type: z.literal('INDIVIDUAL'),
@@ -30,7 +40,7 @@ export const zPayload = z.strictObject({
   })
   // Sofia postcode check
   .refine(data => {
-    if (data.address.includes('Sofia') && data.postcode !== 1000) return false
+    if (data.address.includes('Sofia') && data.postcode !== '1000') return false // todo check '1000'
   }, {
     message: 'The provided postcode does not match the address',
     path: ['postcode']
