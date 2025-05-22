715954895d71dcd33ab381145ad0a8f21667b468
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-22 07:48:04 +0300

    :zap: Add EGN validation

--- src/types/common.ts
+++ src/types/common.ts
@@ -5,20 +5,44 @@ export const zName = z.string()
   .min(2, { message: 'Name must be at least 2 characters long' })
   .max(64, { message: 'Name must be at most 64 characters long' })
   .regex(/^[A-Za-z' -]+$/, { message: 'Only letters, apostrophes, hyphens, and spaces are allowed' }) // Regex supports letters, ' - and space
+
 export const zEGN = z.string()
-  .length(10, { message: 'Maximum length of EGN is 10 characters' }) // .regex(/^[0-9]+$/) // todo update regex with EGN rules
+  .length(10, { message: 'Maximum length of EGN is 10 characters' })
+  .regex(/^\d+$/, { message: 'EGN must consist of only numbers' })
+  .refine(egn => { // Used logic from https://en.wikipedia.org/wiki/Unique_citizenship_number
+    // Separate egn into digits
+    const digits = egn.split('').map(Number)
+
+    // EGN weights
+    const weights = [2, 4, 8, 5, 10, 9, 7, 3, 6]
+
+    // Calculate the checksum
+    const checksum = digits.slice(0, 9).reduce(
+      (acc, digit, index) => acc + digit * weights[index], 0
+    ) % 11
+
+    // Validate EGN
+    return checksum === 10 ? digits[9] === 0 : digits[9] === checksum
+  }, { message: 'Provided EGN is invalid' })
+
 export const zAddress = z.string()
   .min(2, { message: 'Address must be at least 2 characters long' })
   .max(255, { message: 'Address must be at most 255 characters long' })
+
 export const zPostcode = z.string()
   .length(4, { message: 'Postcode must be exactly 4 characters long' })
   .regex(/\d{4}$/, { message: 'Postcode must be a 4-digit number' }) // Regex supports only 4 digits
+
 export const zPhoneNumber = z.string()
   .min(9, { message: 'Phone number must be at least 9 characters long' })
   .max(16, { message: 'Phone number must be at most 16 characters long' })
-  .regex(/^(?:\+|00)\d{9,16}$/, { message: 'Phone number must start with + or 00' }) // Starts with + or 00 and followed by 9 to 16 digits
+  .regex(/^(?:\+|00)\d{9,16}$/,
+    { message: 'Phone number must start with + or 00 and be followed by at least 9 digits' }
+  ) // Regex supports only 9-16 digits and starts with + or 00
+
 export const zEmail = z.string()
   .email({ message: 'Email must be a valid email address' })
+
 
 export const zPayload = z.strictObject({
   type: z.literal('INDIVIDUAL'),
